78f4a64b65db247dd275675b36b7890323a3749e
John15321 <zakuzoani@gmail.com>
2019-12-21 00:06:36 +0100

    Notepad Finish

--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,7 @@
 #include <QTextStream>
 #include <QMessageBox>
 #include <QtPrintSupport/QPrintDialog>
+#include <QtPrintSupport/QPrinter>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -24,6 +25,22 @@ private slots:
     void on_actionNew_triggered();
 
     void on_actionOpen_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionPrint_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
 
 private:
     Ui::MainWindow *ui;
